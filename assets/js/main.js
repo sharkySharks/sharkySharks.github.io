fd090d403c064d0250ccd70b81b53496812c71d8
Sharky <kayden.althen@gmail.com>
2020-02-24 21:43:19 -0600

    fix navbar alignment and functionality

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+    updateLinkActivity(window.location.pathname)
+});
+
+/**
+ * links should have the active class in two situations:
+ * 1) link href matches pathname
+ * 2) when a link is clicked
+ * 
+ * All other links should not have the active class
+ */
+const updateLinkActivity = path => {
+    $('li.nav-item a').each((i, el) => {
+        if ($(el).attr("href") === path) {
+            $(el).addClass("active");
+        } else {
+            $(el).removeClass("active");
+        }
+
+        $(el).click(() => {
+            $('li.nav-item a.active').removeClass("active");
+            $(el).addClass("active");
+        })
+    });   
+}
